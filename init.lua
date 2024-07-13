59ab4df0fc047e5829b62f16dbdc07d58e9d20b4
kighte04 <kighte04@gmail.com>
2024-07-14 00:21:30 +0200

    Removed dap, solved some deprecation warnings

--- init.lua
+++ init.lua
@@ -282,19 +282,21 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      require('which-key').add {
+        {
+          { '<leader>c', group = '[C]ode' },
+          { '<leader>d', group = '[D]ocument' },
+          { '<leader>r', group = '[R]ename' },
+          { '<leader>s', group = '[S]earch' },
+          { '<leader>w', group = '[W]orkspace' },
+          { '<leader>t', group = '[T]oggle' },
+          { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        },
       }
       -- visual mode
-      require('which-key').register({
+      --[[ require('which-key').register({
         ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
+      }, { mode = 'v' }) ]]
     end,
   },
 
@@ -578,7 +580,7 @@ require('lazy').setup({
         pylsp = {},
         rust_analyzer = {
           settings = {
-            ['rust_analyzer'] = {
+            ['rust-analyzer'] = {
               procMacro = {
                 ignored = {
                   leptos_macro = {
@@ -586,6 +588,9 @@ require('lazy').setup({
                   },
                 },
               },
+              --[[ rustfmt = {
+                overrideCommand = { 'leptosfmt', '--stdin', '--rustfmt' },
+              }, ]]
             },
           },
         },
