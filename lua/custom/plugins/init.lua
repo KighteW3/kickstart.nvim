3c87b1159362476f0cd830d53c4b4f4c14dfa747
kighte04 <kighte04@gmail.com>
2025-10-20 17:39:50 +0200

    Background and theming settings

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -10,8 +10,8 @@ return {
     config = function()
       vim.cmd.colorscheme 'gruvbox'
       vim.cmd.color 'gruvbox'
-      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+      -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     end,
   },
   --[[ {
