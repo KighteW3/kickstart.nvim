015e4f6f711cd40d60eee5282880baf004191c02
kighte04 <kighte04@gmail.com>
2026-02-16 01:36:47 +0100

    Added 99 prompt plugin

--- lua/custom/plugins/99.lua
+++ lua/custom/plugins/99.lua
@@ -2,21 +2,20 @@ return {
   'ThePrimeagen/99',
   config = function()
     local _99 = require '99'
-    local ops = require '99.ops'
 
     -- For logging that is to a file if you wish to trace through requests
     -- for reporting bugs, i would not rely on this, but instead the provided
     -- logging mechanisms within 99.  This is for more debugging purposes
-    -- local cwd = vim.uv.cwd()
-    -- local basename = vim.fs.basename(cwd)
+    local cwd = vim.uv.cwd()
+    local basename = vim.fs.basename(cwd)
 
     _99.setup {
-      model = 'opencode/kimi-k2.5-free',
-      -- logger = {
-      --   level = _99.DEBUG,
-      --   path = '/tmp/' .. basename .. '.99.debug',
-      --   print_on_error = true,
-      -- },
+      model = 'openrouter/deepseek/deepseek-v3.2',
+      logger = {
+        level = _99.DEBUG,
+        path = '/tmp/' .. basename .. '.99.debug',
+        print_on_error = true,
+      },
 
       --- A new feature that is centered around tags
       completion = {
@@ -62,11 +61,6 @@ return {
       },
     }
 
-    -- Create your own short cuts for the different types of actions
-    vim.keymap.set('n', '<leader>9f', function()
-      _99.fill_in_function()
-    end)
-
     -- take extra note that i have visual selection only in v mode
     -- technically whatever your last visual selection is, will be used
     -- so i have this set to visual mode so i dont screw up and use an
@@ -78,23 +72,9 @@ return {
       _99.visual()
     end)
 
-    --- Trigger a prompt-based interaction with the 99 AI plugin using visual selection
-    --- This allows you to ask questions or make requests about the selected code
-    vim.keymap.set('v', '<leader>9p', function()
-      _99.visual_prompt(ops)
-    end)
-
     --- if you have a request you dont want to make any changes, just cancel it
     vim.keymap.set('v', '<leader>9s', function()
       _99.stop_all_requests()
-    end)
-
-    --- Example: Using rules + actions for custom behaviors
-    --- Create a rule file like ~/.rules/debug.md that defines custom behavior.
-    --- For instance, a "debug" rule could automatically add printf statements
-    --- throughout a function to help debug its execution flow.
-    vim.keymap.set('n', '<leader>9fd', function()
-      _99.fill_in_function()
     end)
   end,
 }
